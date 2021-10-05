e461734712be48d2dd0cbabc266f7497a4eeb0e9
Mark Rodriguez <markrodriguezaa@gmail.com>
2021-10-05 14:26:10 +0000

    completed first problem and bonus problem

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 //1
-function fizzBuzz(){
-  
+function fizzBuzzz(){
+  for (let i = 1; i<= 100 ; i++){
+      if (i % 3 === 0 && i % 5 === 0){
+          console.log("FizzBuzz");
+      }else if (i % 5 === 0){
+          console.log("Buzz");
+      }else if (i % 3 === 0){
+          console.log("Fizz");
+      }else{
+          console.log(i);
+      }
+    }
 }
 
 //2
 function fizzBuzz(number){
-  
+  let array = [];
+  for(let i =1; i<=number; i++){
+      if (i % 3 === 0 && i % 5 === 0){
+          array.push("FizzBuzz")
+      }else if(i % 3 === 0){
+          array.push("Fizz")
+      }else if(i % 5 === 0){
+          array.push("Buzz")
+      }else{
+          array.push(i)
+      }
+  }
+  return array
 }
 
 
+//fizzBuzzz();
+console.log(fizzBuzz(15));
